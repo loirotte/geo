a977d1bb815ed155f230c744b8639c56ca043289
Philippe Dosch <Philippe.Dosch@loria.fr>
2014-10-09 14:06:12 +0200

    Ajout de 2 constructeurs

--- Point.java
+++ Point.java
@@ -1,12 +1,18 @@
 class Point {
     int x, y;
 
+    // Constructeur par défaut
     Point()
     {
+	x = 0;
+	y = 0;
     }
 
+    // Constructeur de copie
     Point(Point p)
     {
+	x = p.x;
+	y = p.y;
     }
     
     
