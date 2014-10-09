499f0edb92cbf2dab5e06e5eb2ed48b58f791271
Philippe Dosch <Philippe.Dosch@loria.fr>
2014-10-09 14:22:19 +0200

    Revert "Ajout de 2 constructeurs"
    This reverts commit a977d1bb815ed155f230c744b8639c56ca043289.

--- Point.java
+++ Point.java
@@ -1,18 +1,12 @@
 class Point {
     int x, y;
 
-    // Constructeur par défaut
     Point()
     {
-	x = 0;
-	y = 0;
     }
 
-    // Constructeur de copie
     Point(Point p)
     {
-	x = p.x;
-	y = p.y;
     }
     
     
